69fdaf4ec3c3e800b9a58328b5dab6f4aefd4a29
HPrangsuda <k.prangsuda@hotmail.com>
2025-04-30 15:10:42 +0700

    update upload

--- src/main/java/com/machinechecklist/config/FileStorageConfig.java
+++ src/main/java/com/machinechecklist/config/FileStorageConfig.java
@@ -1,5 +1,6 @@
 package com.machinechecklist.config;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,11 +12,11 @@ import java.nio.file.Paths;
 @Configuration
 public class FileStorageConfig implements WebMvcConfigurer {
 
-    private static final String UPLOAD_DIR = "uploads/";
+    private static final String UPLOAD_DIR = "/opt/acme/machine/uploads/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
+        registry.addResourceHandler("/opt/acme/machine/uploads/**")
                 .addResourceLocations("file:" + UPLOAD_DIR);
     }
 
