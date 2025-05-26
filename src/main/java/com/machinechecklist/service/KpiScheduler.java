f6ae92e327646e71617c93d5167a34cf2afb4f40
HPrangsuda <k.prangsuda@hotmail.com>
2025-05-26 11:40:57 +0700

    add kpi

--- src/main/java/com/machinechecklist/service/KpiScheduler.java
+++ src/main/java/com/machinechecklist/service/KpiScheduler.java
@@ -20,7 +20,7 @@ public class KpiScheduler {
     private final MachineRepo machineRepo;
     private final KpiRepo kpiRepo;
 
-    @Scheduled(cron = "0 31 11 26 * ?")
+    @Scheduled(cron = "0 45 11 26 * ?")
     public void createKpiRecords() {
         LocalDate currentDate = LocalDate.now();
         String year = String.valueOf(currentDate.getYear());
