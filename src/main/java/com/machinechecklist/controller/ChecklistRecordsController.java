9151914bfbcc4d2a717f131f902968455999e443
HPrangsuda <k.prangsuda@hotmail.com>
2025-07-02 14:52:25 +0700

    add export excel - checklist

--- src/main/java/com/machinechecklist/controller/ChecklistRecordsController.java
+++ src/main/java/com/machinechecklist/controller/ChecklistRecordsController.java
@@ -3,6 +3,10 @@ package com.machinechecklist.controller;
 import com.machinechecklist.model.*;
 import com.machinechecklist.service.ChecklistRecordsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -59,5 +63,27 @@ public class ChecklistRecordsController {
             @RequestBody ChecklistRecords request) {
         ChecklistRecords updatedChecklist = checklistRecordsService.approveChecklist(checklistId, request);
         return ResponseEntity.ok(updatedChecklist);
+    }
+
+    @GetMapping("/export-excel")
+    public ResponseEntity<ByteArrayResource> exportMachinesToExcel() {
+        try {
+            byte[] excelBytes = checklistRecordsService.exportChecklistToExcel();
+            ByteArrayResource resource = new ByteArrayResource(excelBytes);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=machines.xlsx");
+            headers.add(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate");
+            headers.add(HttpHeaders.PRAGMA, "no-cache");
+            headers.add(HttpHeaders.EXPIRES, "0");
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(excelBytes.length)
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(resource);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
